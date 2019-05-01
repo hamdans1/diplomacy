a164c494d3c292849e01fd5096ac741b34513617
Sami Hamdan <samihamdan00@gmail.com>
2019-05-01 00:22:25 -0400

    added post routes and campaigns#create, still needs rspec success

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -7,6 +7,21 @@ class CampaignsController < ApplicationController
   end
 
   def new
+    @campaign = Campaign.new
+  end
+
+  def create
+    @campaign = Campaign.new
+    @campaign.title = params[:campaign][:title]
+    @campaign.scoring = params[:campaign][:scoring]
+
+    if @campaign.save
+      flash[:notice] = "Post was saved."
+      redirect_to @campaign
+    else
+      flash.now[:alert] = "There was an error saving your post. Please try again."
+      render :new
+    end
   end
 
   def edit
--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -23,13 +23,39 @@ RSpec.describe CampaignsController, type: :controller do
   #   end
   # end
   #
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @campaign" do
+      get :new
+      expect(assigns(:campaign)).not_to be_nil
+    end
+  end
+
+  describe "POST create" do
+    it "increases the number of Campaign by 1" do
+      expect{post :create, campaign: {title: RandomData.random_sentence, scoring: RandomData.random_sentence}}.to change(Campaign, :count).by(1)
+    end
+
+    it "assigns the new campaign to @campaign" do
+      post :create, campaign: {title: RandomData.random_sentence, scoring: RandomData.random_sentence}
+      expect(assigns(:campaign)).to eq Campaign.last
+    end
+
+    it "redirects to the new campaign" do
+      post :create, campaign: {title: RandomData.random_sentence, scoring: RandomData.random_sentence}
+      expect(response).to redirect_to Campaign.last
+    end
+  end
+
   # describe "GET #edit" do
   #   it "returns http success" do
   #     get :edit
