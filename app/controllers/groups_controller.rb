f488297e1222577ccda77247f3f049f387266cfc
Sami Hamdan <samihamdan00@gmail.com>
2019-05-03 09:41:00 -0400

    added new and show methods, views, and testing for group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,4 +3,28 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new
+    @group.name = params[:group][:name]
+    @group.description = params[:group][:description]
+    @group.public = params[:group][:public]
+
+    if @group.save
+      redirect_to @group
+      flash[:notice] = "Group was successfully saved"
+    else
+      flash.now[:alert] = "Error trying to create this Group. Please Try Again"
+      render :new
+    end
+  end
+  
 end
--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -32,4 +32,37 @@ RSpec.describe GroupsController, type: :controller do
       expect(assigns(:group)).to eq (my_group)
     end
   end
+
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "initializes @group" do
+      get :new
+      expect(assigns(:group)).not_to be_nil
+    end
+  end
+
+  describe "POST create" do
+    it "increases the amount of groups by 1" do
+      expect {post :create, :params => { :group => {name: RandomData.random_sentence, description: RandomData.random_sentence}}}.to change(Group, :count).by(1)
+    end
+
+    it "assigns Group.last to @group" do
+      post :create, :params => {:group => {name: RandomData.random_sentence, description: RandomData.random_sentence}}
+      expect(assigns(:group)).to eq Group.last
+    end
+
+    it "redirects to the newly created group" do
+      post :create, :params => {:group => {name: RandomData.random_sentence, description: RandomData.random_sentence}}
+      expect(response).to redirect_to Group.last
+    end
+  end
 end
